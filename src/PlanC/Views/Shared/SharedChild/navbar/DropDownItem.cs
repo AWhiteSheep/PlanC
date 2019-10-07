7f87bd545dc2d772e4a57ae939753d85022cb9e5
awhitesheep <yanharc@gmail.com>
2019-10-07 18:22:43 -0400

    Ajout du menu de navigation, ajout du fichier json

---
+++ src/PlanC/Views/Shared/SharedChild/navbar/DropDownItem.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlanC.Views.Shared.SharedChild.navbar
+{
+    public class DropDownItem : LinkItem
+    {
+        public List<LinkItem> Items { get; set; }
+
+        public DropDownItem()
+        {
+        }
+        
+        public DropDownItem(string display, string redirect, List<LinkItem> dropDownLinkItem) : base(display, redirect)
+        {
+            Items = new List<LinkItem>();
+            foreach (LinkItem item in dropDownLinkItem)
+            {
+                AddItem(item);
+            }
+        }
+
+        public void AddItem(LinkItem newItem)
+        {
+            newItem.parent = this;
+            Items.Add(newItem);
+        }
+    }
+}
---
+++ src/PlanC/Views/Shared/SharedChild/navbar/LinkItem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlanC.Views.Shared.SharedChild.navbar
+{
+    public class LinkItem : Item
+    {
+        public DropDownItem parent { get; set; }
+        public string Text { get; set; }
+        public string Url { get; set; }
+
+        public LinkItem()
+        {
+        }
+
+        public LinkItem(string display, string redirect)
+        {
+            Text = display;
+            Url = redirect;
+        }
+
+        public LinkItem(string display, string redirect, DropDownItem dropDownItem)
+        {
+            Text = display;
+            Url = redirect;
+            parent = dropDownItem;
+        }
+
+        public string Display()
+        {
+            return Text;
+        }
+
+        public string RedirectTo()
+        {
+            return Url;
+        }
+    }
+}
