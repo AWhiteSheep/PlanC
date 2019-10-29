6ec48d2319295590343146b0ec6f28379ebcfa5d
awhitesheep <yanharc@gmail.com>
2019-10-29 15:16:37 -0400

    Design pour page du personnel polie

--- src/PlanC.Web.Client/Pages/Index.cshtml.cs
+++ src/PlanC.Web.Client/Pages/Index.cshtml.cs
@@ -17,9 +17,9 @@ namespace PlanC.Web.Client.Pages
             _logger = logger;
         }
 
-        public void OnGet()
+        public IActionResult OnGet()
         {
-
+            return Redirect("/personnel");
         }
     }
 }
