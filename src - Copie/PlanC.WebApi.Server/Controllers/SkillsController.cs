0bccf3df8d87b900db455020c79babad27cd8c80
awhitesheep <yanharc@gmail.com>
2019-11-04 15:14:35 -0500

    competences scroll and search debut

---
+++ src - Copie/PlanC.WebApi.Server/Controllers/SkillsController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PlanC.WebApi.Models;
+using PlanC.WebApi.Server.DataAccess;
+
+namespace PlanC.WebApi.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SkillsController : ControllerBase
+    {
+        private readonly PCU001Context _context;
+
+        public SkillsController(PCU001Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Skills
+        [HttpGet]
+        [EnableQuery]
+        public IQueryable<Skill> GetSkills()
+        {
+            return _context.Skills;
+        }
+
+        // GET: api/Skills/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSkill([FromRoute] string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var skill = await _context.Skills.FindAsync(id);
+
+            if (skill == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(skill);
+        }
+
+        // PUT: api/Skills/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSkill([FromRoute] string id, [FromBody] Skill skill)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != skill.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(skill).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SkillExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Skills
+        [HttpPost]
+        public async Task<IActionResult> PostSkill([FromBody] Skill skill)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Skills.Add(skill);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (SkillExists(skill.Id))
+                {
+                    return new StatusCodeResult(StatusCodes.Status409Conflict);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetSkill", new { id = skill.Id }, skill);
+        }
+
+        // DELETE: api/Skills/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSkill([FromRoute] string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var skill = await _context.Skills.FindAsync(id);
+            if (skill == null)
+            {
+                return NotFound();
+            }
+
+            _context.Skills.Remove(skill);
+            await _context.SaveChangesAsync();
+
+            return Ok(skill);
+        }
+
+        private bool SkillExists(string id)
+        {
+            return _context.Skills.Any(e => e.Id == id);
+        }
+    }
+}
