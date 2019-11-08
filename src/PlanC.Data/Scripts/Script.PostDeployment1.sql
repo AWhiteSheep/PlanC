2c20cb74d3f4de463987214615a1a74a64656dea
awhitesheep <yanharc@gmail.com>
2019-11-08 11:53:37 -0500

    Ajout de la liste des départements et css formating pour les titres

--- src/PlanC.Data/Scripts/Script.PostDeployment1.sql
+++ src/PlanC.Data/Scripts/Script.PostDeployment1.sql
@@ -22,33 +22,55 @@ Modèle de script de post-déploiement
 --------------------------------------------------------------------------------------
 */
 
-INSERT INTO TDPTMNT
-(
-	DPTMNT_TITLE,
-	DPTMNT_PLCY,
-	TRK_UID
-)
-VALUES
-(
-	'Département d''informatique',
-	'Le français est important. Les retards aussi',
-	'11111'
-),
-(
-	'Département des soins infirmiers',
-	'Le français est important. Les retards ne le sont pas.',
-	'22222'
-),
-(
-	'Département des soins infirmiers',
-	'Le français est important. Les retards ne le sont pas.',
-	'22222'
-),
-(
-	'Département de français',
-	'Le français est important. Les retard blabla',
-	'33333'
-)
+SET IDENTITY_INSERT [dbo].[TDPTMNT] ON
+INSERT INTO [dbo].[TDPTMNT] ([DPTMNT_ID], [DPTMNT_TITLE], [DPTMNT_PLCY], [RCD_CDTTM], [TRK_UID]) VALUES (1, N'Département d''informatique', N'<h6>
+ABSENCE AUX COURS ET LABORATOIRES
+Il</h6>est essentiel que les étudiantes et les étudiants réalisent qu''ils partagent avec l’enseignante la responsabilité de leur apprentissage. Elles et ils doivent assister à toutes les séances de cours et de laboratoire et faire tous leurs travaux afin d’atteindre les compétences souhaitées. « L’étudiante ou étudiant qui s’absente pour un cours ou une partie de période de cours doit récupérer par lui-même les apprentissages manquants. » (PIEA 6.8)
+RETARD AUX COURS
+Il est attendu que l’étudiante ou l’étudiant qui arrive en retard attend la pause (ou que l’enseignante ouvre la porte) pour entrer dans le local afin de ne pas interrompre le cours. Les retardataires ne doivent en aucun cas déranger la classe.
+RETARD DANS LA REMISE DES TRAVAUX
+Les travaux doivent être remis très spécifiquement selon les modalités énoncées (par écrit, par LÉA, vérification sur le poste directement, etc.). Tout retard à la remise de travaux selon les modalités énoncées entraîne automatiquement la note de zéro (0).
+ABSENCE À UNE ÉVALUATION
+« L’étudiante ou étudiant qui ne se présente pas à une évaluation certificative (examen, remise de travaux, etc.) prévue au calendrier obtient la note « 0 » (zéro) à moins qu’il ne justifie son absence à l’enseignante ou enseignant. Si l’enseignante ou enseignant accepte la justification, une reprise de l’évaluation sommative manquée est accordée à l’étudiante ou étudiant. Les modalités de reprise de l’évaluation sont alors déterminées par l''enseignante ou enseignant. » (PIEA 6.4)
+PLAGIAT
+Toute personne prise à plagier, ou qui en a été complice, se voit automatiquement attribuer la note de zéro (0) pour l’activité. Il est à noter que « l’enseignante ou enseignant a la responsabilité de dénoncer les cas de fraude scolaire et de plagiat. » (PIEA 6.9)
+RÉVISION DE NOTES
+L’étudiante ou l’étudiant qui n''est pas satisfait de son évaluation (et non pas de sa note) peut demander une révision de note au service de l''organisation scolaire dans les délais prévus par celle-ci. Advenant une erreur évidente (travail non comptabilisé, erreur de saisie...) il peut aussi en aviser immédiatement l’enseignante qui fera la correction.
+QUALITÉ DU FRANÇAIS
+Pour tous les travaux écrits et les examens, 10% de la note finale est réservée à la qualité du français (orthographe, grammaire, syntaxe, etc.). 
+CONSOMMATION DE NOURRITURE ET BOISSONS
+Aucune nourriture (collation et repas) ne sera tolérée dans les laboratoires informatiques. Les seuls breuvages acceptés sont les suivants : eau, jus, café et autres du même genre. Ils doivent être dans une bouteille fermée ou dans un gobelet antifuite.
+UTILISATION ET RANGEMENT DU MATÉRIEL
+Il est strictement interdit de déplacer ou débrancher le matériel (écrans, ordinateurs, souris, câble réseau du cégep, etc.) informatique qui se trouve dans la classe sans l’autorisation de l’enseignant.  Si l’enseignant l’autorise, le matériel installé devra être enlevé à la fin de chaque cours, et le poste de travail devra être remis à son état initial.
+', N'2019-11-08 09:33:12', N'11111')
+INSERT INTO [dbo].[TDPTMNT] ([DPTMNT_ID], [DPTMNT_TITLE], [DPTMNT_PLCY], [RCD_CDTTM], [TRK_UID]) VALUES (2, N'Département des soins infirmiers', N'<h6>ABSENCE AUX COURS ET LABORATOIRES</h6>Il est essentiel que les étudiantes et les étudiants réalisent qu''ils partagent avec l’enseignante la responsabilité de leur apprentissage. Elles et ils doivent assister à toutes les séances de cours et de laboratoire et faire tous leurs travaux afin d’atteindre les compétences souhaitées. « L’étudiante ou étudiant qui s’absente pour un cours ou une partie de période de cours doit récupérer par lui-même les apprentissages manquants. » (PIEA 6.8)
+<h6>RETARD AUX COURS</h6>Il est attendu que l’étudiante ou l’étudiant qui arrive en retard attend la pause (ou que l’enseignante ouvre la porte) pour entrer dans le local afin de ne pas interrompre le cours. Les retardataires ne doivent en aucun cas déranger la classe.
+<h6>RETARD DANS LA REMISE DES TRAVAUX</h6>Les travaux doivent être remis très spécifiquement selon les modalités énoncées (par écrit, par LÉA, vérification sur le poste directement, etc.). Tout retard à la remise de travaux selon les modalités énoncées entraîne automatiquement la note de zéro (0).
+<h6>ABSENCE À UNE ÉVALUATION</h6>« L’étudiante ou étudiant qui ne se présente pas à une évaluation certificative (examen, remise de travaux, etc.) prévue au calendrier obtient la note « 0 » (zéro) à moins qu’il ne justifie son absence à l’enseignante ou enseignant. Si l’enseignante ou enseignant accepte la justification, une reprise de l’évaluation sommative manquée est accordée à l’étudiante ou étudiant. Les modalités de reprise de l’évaluation sont alors déterminées par l''enseignante ou enseignant. » (PIEA 6.4)
+', N'2019-11-08 09:33:12', N'22222')
+INSERT INTO [dbo].[TDPTMNT] ([DPTMNT_ID], [DPTMNT_TITLE], [DPTMNT_PLCY], [RCD_CDTTM], [TRK_UID]) VALUES (3, N'Département des soins infirmiers', N'ORGANISATION
+
+L’élève a la responsabilité d’assurer sa présence en classe. L’élève qui arrive en retard au cours doit attendre la pause pour entrer, à moins qu’il y soit invité par l’enseignant.
+
+Dans le cas d’une absence motivée, vous êtes invité à informer votre enseignant par COURRIER ÉLECTRONIQUE le plus rapidement possible.
+
+
+JEUX ET CLAVARDAGE EN CLASSE
+
+Il est essentiel que l’élève participe de façon active et qu’un climat propice à l’apprentissage soit maintenu dans la classe. Dans cette optique, les jeux, le clavardage (chat) et toute autre distraction cybernétique sont interdits en tout temps durant les cours (incluant les pauses). En cas de non-respect de cette règle, l’expulsion immédiate du local de l’étudiant pour le reste de la rencontre (séance de cours) sera imposée par l’enseignant. En cas de récidive par l’étudiant, une convocation avec le comité de discipline sera prévue avec les sanctions appropriées.
+', N'2019-11-08 09:33:12', N'22222')
+INSERT INTO [dbo].[TDPTMNT] ([DPTMNT_ID], [DPTMNT_TITLE], [DPTMNT_PLCY], [RCD_CDTTM], [TRK_UID]) VALUES (4, N'Département de français', N'ORGANISATION
+
+L’élève a la responsabilité d’assurer sa présence en classe. L’élève qui arrive en retard au cours doit attendre la pause pour entrer, à moins qu’il y soit invité par l’enseignant.
+
+Dans le cas d’une absence motivée, vous êtes invité à informer votre enseignant par COURRIER ÉLECTRONIQUE le plus rapidement possible.
+
+
+JEUX ET CLAVARDAGE EN CLASSE
+
+Il est essentiel que l’élève participe de façon active et qu’un climat propice à l’apprentissage soit maintenu dans la classe. Dans cette optique, les jeux, le clavardage (chat) et toute autre distraction cybernétique sont interdits en tout temps durant les cours (incluant les pauses). En cas de non-respect de cette règle, l’expulsion immédiate du local de l’étudiant pour le reste de la rencontre (séance de cours) sera imposée par l’enseignant. En cas de récidive par l’étudiant, une convocation avec le comité de discipline sera prévue avec les sanctions appropriées.
+', N'2019-11-08 09:33:12', N'33333')
+
 GO
 
 --TODO : Attent minute. Ne serait-il pas mieux que ce soient les cours qui soient associés aux département, plutôt que les programmes?
