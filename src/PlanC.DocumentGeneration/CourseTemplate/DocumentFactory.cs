50613593d25bb79a8af19f81b89f83d017959362
mcdenis <maxecole@hotmail.com>
2019-11-19 20:46:53 -0500

    Continué génération de document

--- src/PlanC.DocumentGeneration/CourseTemplate/DocumentFactory.cs
+++ src/PlanC.DocumentGeneration/CourseTemplate/DocumentFactory.cs
@@ -9,7 +9,7 @@ using System.Text;
 
 namespace PlanC.DocumentGeneration.CourseTemplate
 {
-    public class DocumentFactory
+    public static class DocumentFactory
     {
         /// <summary>
         ///     Creates a blank <see cref="WordprocessingDocument"/> for a course template.
@@ -17,32 +17,13 @@ namespace PlanC.DocumentGeneration.CourseTemplate
         /// <param name="documentStream">
         ///     The stream that the document should be written to.
         /// </param>
-        /// <param name="isEditable">
-        ///     Whether or not the returned document is editable or read-only.
-        /// </param>
-        /// <param name="settings"></param>
         /// <returns>
-        ///     A new <see cref="WordprocessingDocument"/> wrapping the new document on the stream.
+        ///     A new editable <see cref="WordprocessingDocument"/> wrapping the new document on the stream.
         /// </returns>
-        public static WordprocessingDocument Create(Stream documentStream, bool isEditable = true)
+        public static WordprocessingDocument Create(Stream documentStream, string author = null)
         {
-            Contract.Requires(documentStream != null);
-            
-            WordprocessingDocument document = null;
-            using (var template = WordprocessingDocument.Open(@"J:\À conserver\Session A2019\Projet\PlanC\src\PlanC.DocumentGeneration\CourseTemplate\DocumentTemplate.dotx", false))
-            {
-                //Creates a copy of the template
-                document = (WordprocessingDocument)template.Clone(documentStream, isEditable);
-            }
-
-            //Transform the template into a document.
-            document.ChangeDocumentType(WordprocessingDocumentType.Document);
-
-            document.PackageProperties.Created = DateTime.Now;
-            document.PackageProperties.Creator = "Moi";
-            document.PackageProperties.Revision = null;
-
-            return document;
+            return OpenXmlHelpers.CreateFromTemplate(
+                @"J:\À conserver\Session A2019\Projet\PlanC\src\PlanC.DocumentGeneration\CourseTemplate\DocumentTemplate.dotx", documentStream, author);
         }
     }
 }
