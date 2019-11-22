e169f1fa7a24cbec294894f870cd5b421612e4cc
awhitesheep <yanharc@gmail.com>
2019-11-22 06:31:49 -0500

    Scaffolding scope plan-cadre

---
+++ src/PlanC.Client/Pages/PlanCadre/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PlanC.Client.Data;
+using PlanC.EntityDataModel;
+
+namespace PlanC.Client.Pages.PlanCadre
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly PlanC.Client.Data.PCU001Context _context;
+
+        public DeleteModel(PlanC.Client.Data.PCU001Context context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public PlansCadres PlansCadres { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            PlansCadres = await _context.PlansCadres.FirstOrDefaultAsync(m => m.CoursId == id);
+
+            if (PlansCadres == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            PlansCadres = await _context.PlansCadres.FindAsync(id);
+
+            if (PlansCadres != null)
+            {
+                _context.PlansCadres.Remove(PlansCadres);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ src/PlanC.Client/Pages/PlanCadre/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PlanC.Client.Data;
+using PlanC.EntityDataModel;
+
+namespace PlanC.Client.Pages.PlanCadre
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly PlanC.Client.Data.PCU001Context _context;
+
+        public DetailsModel(PlanC.Client.Data.PCU001Context context)
+        {
+            _context = context;
+        }
+
+        public PlansCadres PlansCadres { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            PlansCadres = await _context.PlansCadres.FirstOrDefaultAsync(m => m.CoursId == id);
+
+            if (PlansCadres == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
