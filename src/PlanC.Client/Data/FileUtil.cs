e2f8bce4f7c77608d9fb74fa4da763dbbff7a7f3
awhitesheep <yanharc@gmail.com>
2019-12-16 18:22:40 -0500

    Download and open

---
+++ src/PlanC.Client/Data/FileUtil.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlanC.Client.Data
+{
+    public static class FileUtil
+    {
+        /// <summary>
+        /// File upload avec js runtime
+        /// </summary>
+        /// <param name="js"></param>
+        /// <param name="filename"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static ValueTask<object> SaveAs(this IJSRuntime js, string filename, byte[] data)
+            => js.InvokeAsync<object>(
+                "saveAsFile",
+                filename,
+                Convert.ToBase64String(data)); 
+        
+        // return une demande invke avec jsRuntime pour téléchargé du côté client
+    }
+}
