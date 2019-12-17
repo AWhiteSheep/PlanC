351e757e73e2ee49e78302a537767d285e24c740
awhitesheep <yanharc@gmail.com>
2019-12-16 21:32:03 -0500

    merge?

--- src/PlanC.EntityDataModel/AspNetUsers.cs
+++ src/PlanC.EntityDataModel/AspNetUsers.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -7,21 +8,13 @@ using System.Text.Json.Serialization;
 
 namespace PlanC.EntityDataModel
 {
-<<<<<<< HEAD:src/PlanC.EntityDataModel/AspNetUsers.cs
     public partial class AspNetUsers : IdentityUser
     {
         public AspNetUsers() : base()
-=======
-    public partial class Utilisateurs
-    {
-        public Utilisateurs()
->>>>>>> e6351150fbe926ea9dd68824e6c58e0603ec91b9:src/PlanC.EntityDataModel/Utilisateurs.cs
         {
             DisponibilitesUtilisateur = new HashSet<DisponibilitesUtilisateur>();
-            RolesUtilisateur = new HashSet<RolesUtilisateur>();
         }
 
-<<<<<<< HEAD:src/PlanC.EntityDataModel/AspNetUsers.cs
         public AspNetUsers(string userName) : base(userName)
         {
         }
@@ -29,12 +22,6 @@ namespace PlanC.EntityDataModel
         [Key]
         [StringLength(7)]
         public override string UserName { get; set; }
-=======
-        [Key]
-        [Column("ID")]
-        [StringLength(7)]
-        public string Id { get; set; }
->>>>>>> e6351150fbe926ea9dd68824e6c58e0603ec91b9:src/PlanC.EntityDataModel/Utilisateurs.cs
         [Column("DepartementID")]
         public int? DepartementId { get; set; }
         [Column("GVN_NM")]
@@ -53,14 +40,9 @@ namespace PlanC.EntityDataModel
         public virtual Departements Departement { get; set; }
         [InverseProperty("U")]
         public virtual ICollection<DisponibilitesUtilisateur> DisponibilitesUtilisateur { get; set; }
-<<<<<<< HEAD:src/PlanC.EntityDataModel/AspNetUsers.cs
-
         // à ne pas se référer n'est pas la clé primaire mais bien un clef de récupration
         [IgnoreDataMember]
         public override string Id { get; set; } // FALLBACK KEY
-=======
-        [InverseProperty("Utilisateur")]
-        public virtual ICollection<RolesUtilisateur> RolesUtilisateur { get; set; }
->>>>>>> e6351150fbe926ea9dd68824e6c58e0603ec91b9:src/PlanC.EntityDataModel/Utilisateurs.cs
+
     }
 }
