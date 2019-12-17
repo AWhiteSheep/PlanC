0f2e10efd6ad91d940da8dcd76e6901431d2a43d
awhitesheep <yanharc@gmail.com>
2019-12-17 07:05:35 -0500

    wor processing html to word depeencies

--- src/PlanC.Client/Data/FileUtil.cs
+++ src/PlanC.Client/Data/FileUtil.cs
@@ -1,8 +1,14 @@
 ﻿using Microsoft.JSInterop;
+using Syncfusion.EJ;
+using Syncfusion.EJ.Export;
+using Syncfusion.Compression;
+using Syncfusion.DocIO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Syncfusion.DocIO.DLS;
+using System.IO;
 
 namespace PlanC.Client.Data
 {
@@ -19,8 +25,24 @@ namespace PlanC.Client.Data
             => js.InvokeAsync<object>(
                 "saveAsFile",
                 filename,
-                Convert.ToBase64String(data)); 
-        
+                Convert.ToBase64String(data));
+
         // return une demande invke avec jsRuntime pour téléchargé du côté client
+
+        public static WordDocument GetDocument(string htmlText)
+        {
+            // renvoit du word qui était en fait du html
+            WordDocument document = null;
+            MemoryStream stream = new MemoryStream();
+            StreamWriter writer = new StreamWriter(stream, System.Text.Encoding.Default);
+            htmlText = htmlText.Replace("\"", "'");
+            XmlConversion XmlText = new XmlConversion(htmlText);
+            XhtmlConversion XhtmlText = new XhtmlConversion(XmlText);
+            writer.Write(XhtmlText.ToString());
+            writer.Flush();
+            stream.Position = 0;
+            document = new WordDocument(stream, FormatType.Html, XHTMLValidationType.None);
+            return document;
+        }
     }
 }
