834166e16476c972c83b8beb552c802223b7c5f3
awhitesheep <yanharc@gmail.com>
2019-12-18 01:21:14 -0500

    Plancours OK

--- src/PlanC.Client/Startup.cs
+++ src/PlanC.Client/Startup.cs
@@ -33,8 +33,6 @@ namespace PlanC.Client
             // Ajout de la dbContext
             services.AddDbContext<PCU001Context>(options => 
                 options.UseSqlServer(Configuration.GetConnectionString("RDS_PCU001")));
-            //options.UseSqlServer("Data Source=database-1.cai5lbxs9ofy.us-east-1.rds.amazonaws.com,1433;User ID=dbo802668235;Password=Nemesis2123%*;Initial Catalog=PCU001;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"));
-
 
             // ajout du identity store et provider
             services.AddIdentity<AspNetUsers, IdentityRole>(options => { 
@@ -59,32 +57,12 @@ namespace PlanC.Client
                 options.SlidingExpiration = true;
             });
 
-
             services.AddRazorPages();
-            services.AddServerSideBlazor().AddCircuitOptions(options => { options.DetailedErrors = true; });
-            services.AddServerSideBlazor().AddHubOptions(o =>
+            services.AddServerSideBlazor().AddCircuitOptions(options => { options.DetailedErrors = true; })
+                .AddHubOptions(o =>
             {
                 o.MaximumReceiveMessageSize = 102400000;
             });
-            services.AddMvc();
-
-            services.AddRazorPages();
-            services.AddServerSideBlazor();
-
-
-            // Be Safe – Sanitize Your HTML 
-            services.AddScoped<IHtmlSanitizer, HtmlSanitizer>(x =>
-            {
-                // https://blog.jonblankenship.com/2019/01/27/safely-rendering-markdown-in-blazor/
-                // Configure sanitizer rules as needed here.
-                // For now, just use default rules + allow class attributes
-                var sanitizer = new Ganss.XSS.HtmlSanitizer();
-                sanitizer.AllowedAttributes.ToList().AddRange(new List<string> { "b", "ul", "ol", "li", "p","i", "u", "em", "strong",
-                 "h1","h2","h3","h4","h5","h6","br","hr","blockquote","code", "del", "dl", "dd", "img", "pre", "s", "sup", "sub", "strike",
-                "div"});
-                return sanitizer;
-            });
-            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -100,13 +78,14 @@ namespace PlanC.Client
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseCors(o => o.AllowAnyOrigin()
-                .AllowAnyHeader().AllowAnyMethod());
 
-            app.UseHttpsRedirection();
+
+            app.UseRouting();
             app.UseStaticFiles();
-
-            app.UseRouting();            
+            app.UseCookiePolicy();
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
